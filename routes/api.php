566b5971fa0226a21ca63261c6cc1c449d5c2497
Katerou22 <xkaterou22@gmail.com>
2020-08-12 09:59:46 +0430

    Remove main request

--- routes/api.php
+++ routes/api.php
@@ -1,55 +1,55 @@
 <?php
 
-	use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
 
-	Route::middleware('auth:api')->get('/user', function (Request $request) {
-		return $request->user();
-	});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
-	Route::prefix('countries')->group(function () {
-		Route::get('/', 'CountryController@countries');
+Route::prefix('countries')->group(function () {
+    Route::get('/', 'CountryController@countries');
 
-		Route::prefix('{country}')->group(function () {
-			Route::get('/', 'CountryController@getCountry');
-			Route::get('/news', 'CountryController@getNews');
-			Route::get('/{model}', 'CountryController@getCountryChild');
-
-
-		});
+    Route::prefix('{country}')->group(function () {
+        Route::get('/', 'CountryController@getCountry');
+        Route::get('/news', 'CountryController@getNews');
+        Route::get('/{model}', 'CountryController@getCountryChild');
 
 
-	});
+    });
 
 
-	Route::middleware('auth:api')->group(function () {
-		Route::get('follow/{type}/{id}', 'UserController@toggleFollow');
-		Route::get('like/{type}/{id}', 'UserController@toggleLike');
-		Route::get('bucket', 'UserController@addToBucket');
-		Route::post('/auth/update', 'AuthController@update');
-		Route::prefix('countries')->group(function () {
-
-			Route::prefix('{country}')->group(function () {
-
-				Route::prefix('add')->group(function () {
-
-					Route::post('culturalNote', 'CountryController@addCulturalNote');
-					Route::post('languageTip', 'CountryController@addLanguageTip');
-					Route::post('photos', 'CountryController@addPhotos');
-					Route::post('notToMiss/{type}', 'CountryController@addNotToMiss');
-				});
+});
 
 
-			});
+Route::middleware('auth:api')->group(function () {
+    Route::get('follow/{type}/{id}', 'UserController@toggleFollow');
+    Route::get('like/{type}/{id}', 'UserController@toggleLike');
+    Route::get('bucket', 'UserController@addToBucket');
+    Route::post('/auth/update', 'AuthController@update');
+    Route::prefix('countries')->group(function () {
+
+        Route::prefix('{country}')->group(function () {
+
+            Route::prefix('add')->group(function () {
+
+                Route::post('culturalNote', 'CountryController@addCulturalNote');
+                Route::post('languageTip', 'CountryController@addLanguageTip');
+                Route::post('photos', 'CountryController@addPhotos');
+                Route::post('notToMiss/{type}', 'CountryController@addNotToMiss');
+            });
 
 
-		});
-	});
-	Route::any('/', 'ApiController@main');
+        });
 
 
-	Route::post('/auth/register', 'AuthController@register');
-	Route::post('/auth/login', 'AuthController@login');
-	Route::post('/auth/recover', 'AuthController@login');
+    });
+});
+//	Route::any('/', 'ApiController@main');
+
+
+Route::post('/auth/register', 'AuthController@register');
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/recover', 'AuthController@login');
 
