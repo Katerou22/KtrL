61b8217e3d7b15761d1b08057207805797f6f3bf
Katerou22 <xkaterou22@gmail.com>
2020-08-17 09:15:41 +0430

    Test api

--- app/Http/Utilities/helpers.php
+++ app/Http/Utilities/helpers.php
@@ -210,6 +210,7 @@ function api($data = NULL, $message = 'success', $code = 1000, $http_code = 200)
         'data' => $data,
 
     ];
+    dd(isset($data['resource']));
     if (property_exists('resource', $data) && $data->resource instanceof LengthAwarePaginator) {
         $response['meta']['paginate'] = [
             'total' => $data->total(),
